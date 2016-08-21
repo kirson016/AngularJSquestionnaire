9b75c6b23433fe18244d9c1ff3e5505efe46f69b
kirson <adam.lubanskii@gmail.com>
2016-08-21 17:06:50 +0200

    directives with controller added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
-var app = angular.module('store', []);
+  var app = angular.module('store', []);
+
+app.directive('optionButtons', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'option-buttons.html',
+        controller: function() {
+            app.controller('buttonsController', function() {
+              this.nazwa = gem;
+            });
+        },
+        controllerAs: 'tab'
+    };
+});
+
+var gem = {
+  name:'asdasdasdasd',
+  price: 123123123,
+  desc:'tak jak ja',
+};
+
+app.directive('inputsSystem', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'inputs-system.html',
+        controller: function() {
+            app.controller('inputController', function() {
+
+            });
+        },
+        controllerAs: 'inputs'
+    };
+});
+
+app.controller("StoreController", function() {
+this.product = gem;
+});
