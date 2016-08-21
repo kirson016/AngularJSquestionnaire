334881446816781bee5ee75207fb824f78b1cca7
kirson <adam.lubanskii@gmail.com>
2016-08-21 17:30:56 +0200

    Working directory

--- js/app.js
+++ js/app.js
@@ -1,37 +1,32 @@
-  var app = angular.module('store', []);
+(function() {
+    var app = angular.module('store', []);
 
-app.directive('optionButtons', function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'option-buttons.html',
-        controller: function() {
-            app.controller('buttonsController', function() {
-              this.nazwa = gem;
-            });
-        },
-        controllerAs: 'tab'
+    app.directive('optionButtons', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'option-buttons.html',
+            controller: function() {
+                this.nazwa = gem;
+            },
+            controllerAs: 'tab'
+        };
+    });
+
+    var gem = {
+        name: 'asdasdasdasd',
+        price: 123123123,
+        desc: 'tak jak ja',
     };
-});
 
-var gem = {
-  name:'asdasdasdasd',
-  price: 123123123,
-  desc:'tak jak ja',
-};
+    app.directive('inputsSystem', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'inputs-system.html',
+            controller: function() {
+                this.inpSys = gem;
+            },
+            controllerAs: 'inputs'
+        };
+    });
 
-app.directive('inputsSystem', function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'inputs-system.html',
-        controller: function() {
-            app.controller('inputController', function() {
-
-            });
-        },
-        controllerAs: 'inputs'
-    };
-});
-
-app.controller("StoreController", function() {
-this.product = gem;
-});
+})();
