a63175c9f0441024eef7c7986054236401f62db4
kirson <adam.lubanskii@gmail.com>
2016-08-22 16:46:59 +0200

    ng-show function add'ed

--- js/app.js
+++ js/app.js
@@ -1,22 +1,50 @@
 (function() {
     var app = angular.module('store', []);
 
+    app.controller('personCtrl', function($scope) {
+        $scope.firstName = "John",
+            $scope.lastName = "Doe"
+        $scope.myVar = false;
+        $scope.myVarr = false;
+        $scope.myVarrr = false;
+        $scope.toggle = function() {
+            $scope.myVar = !$scope.myVar;
+            $scope.myVarr = !$scope.myVarr;
+            $scope.myVarrr = !$scope.myVarrr;
+        };
+    });
+
+
     app.directive('optionButtons', function() {
         return {
             restrict: 'E',
             templateUrl: 'option-buttons.html',
             controller: function() {
-                this.nazwa = gem;
+                this.history = false;
+                this.questionnaire = true;
+                this.inProgress = false;
+
+                this.isSelected = function(clicked) {
+                    if (clicked == 'history') {
+                        this.history = true;
+                        this.questionnaire = false;
+                        this.inProgress = false;
+                    } else if (clicked === 'newQuestionary') {
+                      this.history = false;
+                      this.questionnaire = true;
+                      this.inProgress = false;
+                    } else {
+                      this.history = false;
+                      this.questionnaire = false;
+                      this.inProgress = true;
+                    }
+                };
+
+
             },
             controllerAs: 'tab'
         };
     });
-
-    var gem = {
-        name: 'asdasdasdasd',
-        price: 123123123,
-        desc: 'tak jak ja',
-    };
 
     app.directive('inputsSystem', function() {
         return {
